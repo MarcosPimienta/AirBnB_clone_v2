a0d0e7d0d6b0be738a9fcc33639e1fcc874e558c
MarcosPimienta <1676@holbertonschool.com>
2020-12-22 23:38:31 +0000

    changed code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
 """ Distributes an archive to your
-web servers, using the function do_deploy """
-from fabric.api import *
-from fabric.operations import *
-from os import path
-import os.path
-import os
+web servers, using the function do_deploy: """
 
-env.user = ['ubuntu']
+from fabric.api import *
+from os import path
+
 env.hosts = ['35.237.158.254', '35.243.174.193']
 
 
 def do_deploy(archive_path):
-    """ Prototype do_deploy """
-
-    if (path.exists(archive_path) is not True):
+    """Deploy in the servers"""
+    if not path.exists(archive_path):
         return False
-    Directory = archive_path.replace(".tgz", "")
-    put("archive_path", "/tmp/{}".format(archive_path))
-    run("mkdir -p /data/web_static/releases/{}/".format(Directory))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-        .format(archive_path, Directory))
-    run("rm /tmp/{}/".format(archive_path))
-    run("mv /data/web_static/releases/{}/* /data/web_static/releases/{}/"
-        .format(Directory, Directory))
-    run("rm -rf /data/web_static/releases/{}/web_static"
-        .format(Directory))
+
+    ufile = put(archive_path, '/tmp/')
+    if ufile.failed:
+        return False
+
+    complete = archive_path.split("/")[-1]
+    fileok = complete.split(".")[0]
+
+    run("mkdir -p /data/web_static/releases/{}/".format(fileok))
+    run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
+        format(fileok, fileok))
+
+    run("rm /tmp/{}.tgz".format(fileok))
+
+    run("mv /data/web_static/releases/{}/web_static/* "
+        "/data/web_static/releases/{}/".format(fileok, fileok))
+    run("rm -rf /data/web_static/releases/{}/web_static".
+        format(fileok))
     run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-        .format(Directory))
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+        format(fileok))
     print("New version deployed!")
     return True
