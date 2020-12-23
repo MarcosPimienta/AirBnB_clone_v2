b1790473a09a8ad8123dca8e7b3c23c67d651243
MarcosPimienta <1676@holbertonschool.com>
2020-12-23 04:35:17 +0000

    added sudo perms

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,7 +10,7 @@ env.hosts = ['35.237.158.254', '35.243.174.193']
 
 def do_deploy(archive_path):
     """Deploy in the servers"""
-    if not path.exists(archive_path):
+    if path.exists(archive_path) is False:
         return False
 
     ufile = put(archive_path, '/tmp/')
@@ -20,18 +20,17 @@ def do_deploy(archive_path):
     complete = archive_path.split("/")[-1]
     fileok = complete.split(".")[0]
 
-    run("mkdir -p /data/web_static/releases/{}/".format(fileok))
-    run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
+    run("sudo mkdir -p /data/web_static/releases/{}/".format(fileok))
+    run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
         format(fileok, fileok))
 
-    run("rm /tmp/{}.tgz".format(fileok))
+    run("sudo rm /tmp/{}.tgz".format(fileok))
 
-    run("mv /data/web_static/releases/{}/web_static/* "
+    run("sudo mv /data/web_static/releases/{}/web_static/* "
         "/data/web_static/releases/{}/".format(fileok, fileok))
-    run("rm -rf /data/web_static/releases/{}/web_static".
+    run("sudo rm -rf /data/web_static/releases/{}/web_static".
         format(fileok))
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+    run("sudo rm -rf /data/web_static/current")
+    run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
         format(fileok))
-    print("New version deployed!")
     return True
