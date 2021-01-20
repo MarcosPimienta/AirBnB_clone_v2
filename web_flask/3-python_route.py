9bef22292f04d5d2a44ff403a342d20772e36ec9
MarcosPimienta <1676@holbertonschool.com>
2021-01-19 20:03:42 -0500

    fixed pep8

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def hello_world(text = "is cool"):
+def hello_world(text="is cool"):
     return 'Python {}'.format(text).replace('_', ' ')
 
 
