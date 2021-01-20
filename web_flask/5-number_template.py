afed9159ba56ed3e0f29bb8b78863e1a4dcb1fd5
MarcosPimienta <1676@holbertonschool.com>
2021-01-19 22:06:09 -0500

    html number

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Flask """
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def hello_world(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port="5000")
