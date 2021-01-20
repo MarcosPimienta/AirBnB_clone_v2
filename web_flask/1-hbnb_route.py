e574b974cf1feebf122a09c8eecb23ea38cb46c4
MarcosPimienta <1676@holbertonschool.com>
2021-01-19 19:31:14 -0500

    testing

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask Project"""
+"""Flask """
 from flask import Flask
 
 
@@ -7,10 +7,6 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_world():
-    return 'Hello HBNB!'
-
-
 @app.route('/hbnb', strict_slashes=False)
 def hello_world():
     return 'HBNB'
